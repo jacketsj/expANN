30f46952d89c4ee0940e7f24d5f86f8d8c7f55c2
jacketsj <jacketsj@gmail.com>
2023-08-15 00:53:34 -0700

    Use libeigen in vec.h. Unsurprisingly, it's a fair bit faster.

--- src/vec.h
+++ src/vec.h
@@ -6,22 +6,45 @@
 #include <string>
 #include <vector>
 
+#include <Eigen/Dense>
+
 #include "small_vector.hpp"
+
+template <typename> struct ChooseUnderlying;
+
+template <> struct ChooseUnderlying<float> {
+	using Underlying = Eigen::VectorXf;
+};
+
+template <> struct ChooseUnderlying<double> {
+	using Underlying = Eigen::VectorXd;
+};
+
+template <typename T> struct ChooseUnderlying {
+	using Underlying = gch::small_vector<T>;
+};
 
 template <typename T> class vec {
 	// std::vector<T> internal;
-	gch::small_vector<T> internal;
+	// gch::small_vector<T> internal;
+
+	// Eigen::VectorXf internal;
+	using Underlying = ChooseUnderlying<T>::Underlying;
+	Underlying internal;
 
 public:
 	vec() = default;
 	// vec(size_t dim) : internal(dim) {}
 	vec(const std::vector<T>& v) {
-		for (const auto& val : v)
-			internal.emplace_back(val);
+		internal.resize(v.size());
+		for (size_t i = 0; i < size_t(v.size()); ++i)
+			internal[i] = v[i];
+		// for (const auto& val : v)
+		//	internal.emplace_back(val);
 	}
 	void set_dim(size_t dim) { internal.resize(dim); }
-	size_t size() const { return internal.size(); }
-	size_t dim() const { return internal.size(); }
+	size_t size() const { return size_t(internal.size()); }
+	size_t dim() const { return size(); }
 	std::string to_string() const {
 		std::string ret;
 		for (const auto& val : internal)
@@ -31,20 +54,20 @@ public:
 	T& operator[](size_t i) { return internal[i]; }
 	void operator+=(const vec<T>& oth) {
 		// assert(dim() == oth.dim());
-		for (size_t i = 0; i < internal.size(); ++i)
+		for (size_t i = 0; i < size(); ++i)
 			internal[i] += oth.internal[i];
 	}
 	void operator-=(const vec<T>& oth) {
 		// assert(dim() == oth.dim());
-		for (size_t i = 0; i < internal.size(); ++i)
+		for (size_t i = 0; i < size(); ++i)
 			internal[i] -= oth.internal[i];
 	}
 	void operator*=(const T& val) {
-		for (size_t i = 0; i < internal.size(); ++i)
+		for (size_t i = 0; i < size(); ++i)
 			internal[i] *= val;
 	}
 	void operator/=(const T& val) {
-		for (size_t i = 0; i < internal.size(); ++i)
+		for (size_t i = 0; i < size(); ++i)
 			internal[i] /= val;
 	}
 	vec<T> operator+(const vec<T>& oth) const {
@@ -61,7 +84,7 @@ public:
 	T dot(const vec<T>& oth) const {
 		// assert(dim() == oth.dim());
 		T ans = 0;
-		for (size_t i = 0; i < internal.size(); ++i)
+		for (size_t i = 0; i < size(); ++i)
 			ans += internal[i] * oth.internal[i];
 		return ans;
 	}
@@ -96,7 +119,7 @@ public:
 	}
 	friend void from_json(const nlohmann::json& j, vec<T>& v) {
 		// v.internal = j.get<std::vector<T>>();
-		for (auto& val : j.get<std::vector<T>>())
-			v.internal.push_back(val);
+		auto full_vector = j.get<std::vector<T>>();
+		v = vec<T>(full_vector);
 	}
 };
