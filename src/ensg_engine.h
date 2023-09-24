f2e687f9ec8e76c650b4a6e295911bbd1d8d20c2
jacketsj <jacketsj@gmail.com>
2023-09-24 02:40:54 -0700

    Add initial implementation of ensg_engine (a drastic simplification/hopefully speedup of ehnsw3)

---
+++ src/ensg_engine.h
@@ -0,0 +1,236 @@
+#pragma once
+
+#include <algorithm>
+#include <limits>
+#include <queue>
+#include <random>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "ann_engine.h"
+#include "robin_hood.h"
+
+struct ensg_engine_config {
+	size_t edge_count_mult;
+	size_t num_for_1nn;
+	ensg_engine_config(size_t _edge_count_mult, size_t _num_for_1nn)
+			: edge_count_mult(_edge_count_mult), num_for_1nn(_num_for_1nn) {}
+};
+
+template <typename T>
+struct ensg_engine : public ann_engine<T, ensg_engine<T>> {
+	std::random_device rd;
+	std::mt19937 gen;
+	std::uniform_int_distribution<> int_distribution;
+	size_t starting_vertex;
+	size_t edge_count_mult;
+	size_t num_for_1nn;
+	const size_t num_cuts;
+	ensg_engine(ensg_engine_config conf)
+			: rd(), gen(rd()), int_distribution(0, 1),
+				edge_count_mult(conf.edge_count_mult), num_for_1nn(conf.num_for_1nn),
+				num_cuts(conf.edge_count_mult - 1) {}
+	std::vector<vec<T>> all_entries;
+	robin_hood::unordered_flat_map<size_t, std::vector<size_t>>
+			adj; // vertex -> cut -> outgoing_edge data_index
+	robin_hood::unordered_flat_map<size_t,
+																 std::vector<std::set<std::pair<T, size_t>>>>
+			edge_ranks; // vertex -> furthest connected
+	robin_hood::unordered_flat_map<size_t, std::vector<bool>>
+			e_labels; // vertex -> cut labels (*num_cuts=edge_count_mult-1)
+	void _store_vector(const vec<T>& v);
+	void _build();
+	const std::vector<std::pair<T, size_t>>
+	_query_k_internal(const vec<T>& v, size_t k,
+										std::vector<size_t>& starting_points, bool include_visited);
+	bool is_valid_edge(size_t i, size_t j, size_t bin);
+	void add_edge(size_t i, size_t j, T d);
+	void add_edge_directional(size_t i, size_t j, T d);
+	const std::vector<std::pair<T, size_t>>
+	_query_k_internal_wrapper(const vec<T>& v, size_t k, bool include_visited);
+	std::vector<size_t> _query_k(const vec<T>& v, size_t k);
+	const std::string _name() { return "ENSG Engine"; }
+	const param_list_t _param_list() {
+		param_list_t pl;
+		add_param(pl, edge_count_mult);
+		add_param(pl, num_for_1nn);
+		return pl;
+	}
+	bool generate_elabel() { return int_distribution(gen); }
+};
+
+template <typename T> void ensg_engine<T>::_store_vector(const vec<T>& v) {
+	all_entries.push_back(v);
+}
+
+template <typename T>
+bool ensg_engine<T>::is_valid_edge(size_t i, size_t j, size_t bin) {
+	// the last bin permits any edge
+	if (bin == num_cuts)
+		return true;
+	// an edge is permitted in a bin if it crosses the cut for that bin
+	return e_labels[i][bin] != e_labels[j][bin];
+}
+
+template <typename T>
+void ensg_engine<T>::add_edge_directional(size_t i, size_t j, T d) {
+	// TODO refactor this to be sane
+	// no randomness needed here
+	//
+	// T d = dist(all_entries[i], all_entries[j]);
+	std::vector<size_t> cuts;
+	for (size_t cut = 0; cut <= num_cuts; ++cut)
+		cuts.push_back(cut);
+	std::shuffle(cuts.begin(), cuts.end(), gen);
+	// choose a random sequence of cuts until a cut that allows for (i,j) is found
+	size_t found_cut = num_cuts;
+	for (size_t cut : cuts) {
+		// if (cut == num_cuts || e_labels[i][cut] != e_labels[j][cut]) {
+		if (is_valid_edge(i, j, cut)) {
+			found_cut = cut;
+			break;
+		}
+	}
+	// if the total number of edges would be too large, find all non-empty cuts,
+	// and pick out the one with the longest edge
+	size_t max_cut = 0;
+	T max_cut_val = std::numeric_limits<T>::max();
+	size_t deleted_j = i; // nothing deleted if deleted_j == i
+	if (adj[i].size() + 1 > edge_count_mult) {
+		for (size_t cut : cuts) {
+			size_t sz = edge_ranks[i][cut].size();
+			if (cut == found_cut)
+				sz++;
+			if (sz > 1) {
+				T cur_cut_val = edge_ranks[i][cut].begin()->first;
+				if (cur_cut_val < max_cut_val) {
+					max_cut_val = cur_cut_val;
+					max_cut = cut;
+				}
+			}
+		}
+		// if that longest edge is longer than our candidate edge, make the swap
+		if (-max_cut_val > d) {
+			auto iter = edge_ranks[i][max_cut].begin();
+			size_t edge_index = iter->second;
+			deleted_j = adj[i][edge_index];
+			adj[i][edge_index] = j;
+			edge_ranks[i][max_cut].erase(iter);
+			edge_ranks[i][found_cut].emplace(-d, edge_index);
+		}
+	} else {
+		adj[i].emplace_back(j);
+		edge_ranks[i][found_cut].emplace(-d, adj[i].size() - 1);
+	}
+	// try to bump a bigger edge until stability is reached
+	if (deleted_j != i)
+		add_edge_directional(i, deleted_j, -max_cut_val);
+}
+
+template <typename T> void ensg_engine<T>::add_edge(size_t i, size_t j, T d) {
+	add_edge_directional(i, j, d);
+	add_edge_directional(j, i, d);
+}
+
+template <typename T> void ensg_engine<T>::_build() {
+	assert(all_entries.size() > 0);
+	auto add_vertex_base = [&](size_t v) {
+		edge_ranks[v] = std::vector<std::set<std::pair<T, size_t>>>(num_cuts + 1);
+		for (size_t cut = 0; cut < num_cuts; ++cut)
+			e_labels[v].emplace_back(generate_elabel());
+	};
+	starting_vertex = 0;
+	add_vertex_base(0);
+	auto improve_vertex_edges = [&](size_t v) {
+		// get current approx kNN
+		std::vector<std::pair<T, size_t>> kNN =
+				_query_k_internal_wrapper(all_entries[i], edge_count_mult, true);
+		// add all the found neighbours as edges (if they are good)
+		sort(kNN.begin(), kNN.end());
+		for (auto [d, j] : kNN) {
+			add_edge(i, j, d);
+		}
+	};
+
+	for (size_t i = 1; i < all_entries.size(); ++i) {
+		if (i % 5000 == 0)
+			std::cerr << "Built " << double(i) / double(all_entries.size()) * 100
+								<< "%" << std::endl;
+
+		add_vertex_base(i);
+		improve_vertex_edges(i);
+		// TODO for dynamic version, call improve_vertex_edges on a vertex after a
+		// number of operations following the insertion proportional to the size of
+		// the data structure at the time of the insertion
+		//
+		// TODO for dynamic version, keep a random sample point as a starting point
+		// (i.e. if existing starting point is deleted, sample another)
+		// potentially use a few different starting points, for better sampling
+	}
+}
+template <typename T>
+const std::vector<std::pair<T, size_t>>
+ensg_engine<T>::_query_k_internal(const vec<T>& v, size_t k,
+																	std::vector<size_t>& starting_points,
+																	bool include_visited) {
+	std::priority_queue<std::pair<T, size_t>> top_k;
+	std::priority_queue<std::pair<T, size_t>> to_visit;
+	robin_hood::unordered_flat_map<size_t, T> visited;
+	auto visit = [&](T d, size_t u) {
+		bool is_good =
+				!visited.contains(u) && (top_k.size() < k || top_k.top().first > d);
+		visited[u] = d;
+		if (is_good) {
+			top_k.emplace(d, u);		 // top_k is a max heap
+			to_visit.emplace(-d, u); // to_visit is a min heap
+		}
+		if (top_k.size() > k)
+			top_k.pop();
+		return is_good;
+	};
+	for (auto& sp : starting_points)
+		visit(dist(v, all_entries[sp]), sp);
+	while (!to_visit.empty()) {
+		T nd;
+		size_t cur;
+		std::tie(nd, cur) = to_visit.top();
+		if (top_k.size() == k && -nd > top_k.top().first)
+			// everything neighbouring current best set is already evaluated
+			break;
+		to_visit.pop();
+		for (const auto& u : adj[cur]) {
+			T d_next = dist(v, all_entries[u]);
+			visit(d_next, u);
+		}
+	}
+	std::vector<std::pair<T, size_t>> ret;
+	while (!top_k.empty()) {
+		ret.push_back(top_k.top());
+		top_k.pop();
+	}
+	if (include_visited) {
+		ret.clear();
+		for (auto [node, d] : visited)
+			ret.emplace_back(d, node);
+	}
+	reverse(ret.begin(), ret.end()); // sort from closest to furthest
+	return ret;
+}
+
+template <typename T>
+const std::vector<std::pair<T, size_t>>
+ensg_engine<T>::_query_k_internal_wrapper(const vec<T>& v, size_t k,
+																					bool include_visited) {
+	return _query_k_internal(v, k, {starting_vertex}, include_visited);
+}
+
+template <typename T>
+std::vector<size_t> ensg_engine<T>::_query_k(const vec<T>& v, size_t k) {
+	auto ret_combined = _query_k_internal_wrapper(v, k * num_for_1nn, false)[0];
+	ret_combined.resize(std::min(k, ret_combined.size()));
+	auto ret = std::vector<size_t>(ret_combined.size());
+	for (size_t i = 0; i < ret.size(); ++i)
+		ret[i] = ret_combined[i].second;
+	return ret;
+}
