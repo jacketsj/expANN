4f42dac57fdfce67188b8007672fb1c653f62d15
jacketsj <jacketsj@gmail.com>
2023-10-07 22:43:51 -0700

    Finish figuring out a magic number... 4 is just a good ratio apparently

--- src/pyplotter.py
+++ src/pyplotter.py
@@ -91,7 +91,7 @@ class AnnoteFinder(object):
 #f = open('./data/sift1m_full/data/all.json')
 #f = open('./data/sift1m_full_k10/data/all.json')
 #f = open('./data/synthetic_uniform_sphere_n90000_dim16_m600_k10/data/all.json')
-f = open('./data/synthetic_uniform_sphere_n56000_dim128_m400_k10/data/latest.json')
+f = open('./data/synthetic_uniform_sphere_n56000_dim128_m400_k10/data/all.json')
 #f = open('./data/synthetic_uniform_sphere_n56000_dim64_m400_k10/data/latest.json')
 #f = open('./data/synthetic_uniform_sphere_n56000_dim128_m400_k10/data/some-combined-stuff.json')
 #f = open('./data/synthetic_uniform_sphere_n56000_dim128_m400_k10/data/hnsw2_vs_ehnsw2_vs_filterehnsw.json')
