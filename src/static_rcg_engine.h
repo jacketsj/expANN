26290a6ee32dc6cf19a87e733db8264bcbd53bbc
jacketsj <jacketsj@gmail.com>
2023-10-20 13:04:27 -0700

    Add root nodes to metanode (should be replaced with metanodes of cluster centres), plus add query_k_at_metanode implementation

--- src/static_rcg_engine.h
+++ src/static_rcg_engine.h
@@ -33,26 +33,8 @@ struct static_rcg_engine_config {
 
 template <typename T>
 struct static_rcg_engine : public ann_engine<T, static_rcg_engine<T>> {
-	std::random_device rd;
-	std::mt19937 gen;
-	std::uniform_int_distribution<> distribution;
-	size_t starting_vertex;
-	size_t M;
-	size_t cluster_overlap;
-	size_t C;
-	size_t brute_force_size;
-	size_t ef_search_mult;
-	size_t ef_construction;
-	static_rcg_engine(static_rcg_engine_config conf)
-			: rd(), gen(0), distribution(0, conf.C - 1), M(conf.M),
-				cluster_overlap(conf.cluster_overlap), C(conf.C),
-				brute_force_size(conf.brute_force_size),
-				ef_search_mult(conf.ef_search_mult),
-				ef_construction(conf.ef_construction) {}
-	std::vector<vec<T>> all_entries;
-	void _store_vector(const vec<T>& v);
-	void _build();
 	struct metanode {
+		size_t starting_vertex;
 		robin_hood::unordered_flat_map<size_t, size_t>
 				to_local_index;									 // global index -> metanode local index
 		std::vector<size_t> to_global_index; // metanode local index -> global index
@@ -65,7 +47,28 @@ struct static_rcg_engine : public ann_engine<T, static_rcg_engine<T>> {
 				to_cluster_indices; // metanode local index ->
 														// metanode centre indices (membership)
 	};
-	std::vector<size_t> _query_k(const vec<T>& v, size_t k);
+	std::random_device rd;
+	std::mt19937 gen;
+	std::uniform_int_distribution<> distribution;
+	size_t M;
+	size_t cluster_overlap;
+	size_t C;
+	size_t brute_force_size;
+	size_t ef_search_mult;
+	size_t ef_construction;
+	metanode root;
+	static_rcg_engine(static_rcg_engine_config conf)
+			: rd(), gen(0), distribution(0, conf.C - 1), M(conf.M),
+				cluster_overlap(conf.cluster_overlap), C(conf.C),
+				brute_force_size(conf.brute_force_size),
+				ef_search_mult(conf.ef_search_mult),
+				ef_construction(conf.ef_construction), root() {}
+	std::vector<vec<T>> all_entries;
+	void _store_vector(const vec<T>& v);
+	void _build();
+	std::vector<size_t> query_k_at_metanode(metanode& mn, const vec<T>& q,
+																					size_t k);
+	std::vector<size_t> _query_k(const vec<T>& q, size_t k);
 	const std::string _name() { return "Static RCG Engine"; }
 	const param_list_t _param_list() {
 		param_list_t pl;
@@ -76,7 +79,6 @@ struct static_rcg_engine : public ann_engine<T, static_rcg_engine<T>> {
 		add_param(pl, ef_construction);
 		return pl;
 	}
-	metanode root;
 };
 
 template <typename T>
@@ -91,6 +93,7 @@ template <typename T> void static_rcg_engine<T>::_build() {
 	{
 		for (size_t i = 0; i < all_entries.size(); ++i)
 			root.to_global_index.emplace_back(i);
+		root.starting_vertex = 0; // arbitrary, should be random (later)
 		to_build.emplace(root);
 	}
 	while (!to_build.empty()) {
@@ -105,6 +108,10 @@ template <typename T> void static_rcg_engine<T>::_build() {
 				cluster_centres.emplace_back(i);
 		}
 		mn.clusters.resize(cluster_centres.size());
+
+		// convert starting vertex to a local index (assumed to be global index on
+		// entry)
+		mn.starting_vertex = mn.to_local_index[mn.starting_vertex];
 
 		if (contents.size() <= brute_force_size) {
 			continue;
@@ -123,18 +130,129 @@ template <typename T> void static_rcg_engine<T>::_build() {
 		for (size_t i = 0; i < contents.size(); ++i) {
 			mn.to_cluster_indices.emplace_back(cluster_engine.query_k(
 					all_entries[mn.to_global_index[i]], cluster_overlap));
+		}
+
+		// ensure each cluster centre is in its own cluster, just in case (since
+		// this is only approximately guaranteed)
+		for (size_t i = 0; i < cluster_centres.size(); ++i) {
+			bool in_clusters = false;
+			for (size_t cluster_index : mn.to_cluster_indices[cluster_centres[i]]) {
+				if (cluster_index == i) {
+					in_clusters = true;
+					break;
+				}
+			}
+			if (!in_clusters) {
+				mn.to_cluster_indices[cluster_centres[i]]
+						.pop_back(); // get rid of the worst one
+				mn.to_cluster_indices[cluster_centres[i]].emplace_back(
+						i); // replace it with i
+			}
+			// initialize cluster starting vertex to a global index
+			mn.clusters[i].starting_vertex = contents[cluster_centres[i]];
+		}
+
+		// actually put everything in the clusters
+		for (size_t i = 0; i < contents.size(); ++i) {
 			for (size_t cluster_index : mn.to_cluster_indices[i])
 				mn.clusters[cluster_index].to_global_index.emplace_back(contents[i]);
 		}
 
 		// enqueue each cluster for building/indexing
-		for (size_t i = 0; i < mn.clusters.size(); ++i)
+		for (size_t i = 0; i < mn.clusters.size(); ++i) {
 			to_build.emplace(mn.clusters[i]);
+		}
 	}
 }
 
 template <typename T>
+std::vector<size_t> static_rcg_engine<T>::query_k_at_metanode(metanode& mn,
+																															const vec<T>& q,
+																															size_t k) {
+	topk_t<T> tk(k);
+	if (mn.to_global_index.size() <= brute_force_size) {
+		for (size_t global_index : mn.to_global_index) {
+			tk.consider(dist2(q, all_entries[global_index]), global_index);
+		}
+		return tk.to_vector();
+	}
+
+	// do traversal to get knn
+	using measured_data = std::pair<T, size_t>;
+	auto worst_elem = [](const measured_data& a, const measured_data& b) {
+		return a.first < b.first;
+	};
+	auto best_elem = [](const measured_data& a, const measured_data& b) {
+		return a.first > b.first;
+	};
+
+	// TODO entry points should be determined by a recursive metanode
+	// be careful with global/local indices
+
+	std::vector<size_t> entry_points = {mn.starting_vertex};
+	std::vector<measured_data> entry_points_with_dist;
+	for (auto& entry_point : entry_points)
+		entry_points_with_dist.emplace_back(dist2(q, all_entries[entry_point]),
+																				entry_point);
+	// candidates stores local indices (so to_cluster_indices can be used)
+	std::priority_queue<measured_data, std::vector<measured_data>,
+											decltype(best_elem)>
+			candidates(entry_points_with_dist.begin(), entry_points_with_dist.end(),
+								 best_elem);
+	// nearest stores global indices (so it can be returned easily)
+	std::priority_queue<measured_data, std::vector<measured_data>,
+											decltype(worst_elem)>
+			nearest(worst_elem);
+	//(entry_points_with_dist.begin(), entry_points_with_dist.end(),
+	// worst_elem);
+	for (auto [d, local_index] : entry_points_with_dist) {
+		nearest.emplace(d, mn.to_global_index[local_index]);
+	}
+
+	while (nearest.size() > k)
+		nearest.pop();
+
+	robin_hood::unordered_flat_set<size_t> visited;
+	robin_hood::unordered_flat_set<size_t> visited_clusters;
+	for (auto& entry_point : entry_points) {
+		visited.insert(entry_point);
+	}
+
+	while (!candidates.empty()) {
+		auto cur = candidates.top();
+		candidates.pop();
+		if (cur.first > nearest.top().first && nearest.size() == k) {
+			break;
+		}
+		for (size_t cluster_index : mn.to_cluster_indices[cur.second]) {
+			if (visited_clusters.contains(cluster_index))
+				continue;
+			visited_clusters.insert(cluster_index);
+			for (size_t global_next :
+					 query_k_at_metanode(mn.clusters[cluster_index], q, k)) {
+				if (!visited.contains(global_next)) {
+					visited.insert(global_next);
+					T d_next = dist2(q, all_entries[global_next]);
+					if (nearest.size() < k || d_next < nearest.top().first) {
+						candidates.emplace(d_next, mn.to_local_index[global_next]);
+						nearest.emplace(d_next, global_next);
+						if (nearest.size() > k)
+							nearest.pop();
+					}
+				}
+			}
+		}
+	}
+	std::vector<size_t> ret;
+	while (!nearest.empty()) {
+		ret.emplace_back(nearest.top().second);
+		nearest.pop();
+	}
+	reverse(ret.begin(), ret.end());
+	return ret;
+}
+
+template <typename T>
 std::vector<size_t> static_rcg_engine<T>::_query_k(const vec<T>& q, size_t k) {
-	// TODO
-	return {};
+	return query_k_at_metanode(root, q, k * ef_search_mult);
 }
