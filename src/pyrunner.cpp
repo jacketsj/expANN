032d3e14f4bc252be41eca79ebd520806fce0703
jacketsj <jacketsj@gmail.com>
2023-10-29 10:11:09 -0700

    Set up pybind compilation

---
+++ src/pyrunner.cpp
@@ -0,0 +1,27 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "ann_engine.h"
+#include "ehnsw_engine_basic_fast_disk.h"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(expann_py, m) {
+	py::class_<vec<float>>(m, "Vec")
+			.def(py::init<>())
+			//.def_readwrite("data", &vec<float>::data);
+			.def("data",
+					 [](vec<float>& v) {
+						 return py::memoryview::from_buffer(v.data(), {v.size()},
+																								{sizeof(float)});
+					 })
+			.def("size", &vec<float>::size);
+
+	py::class_<ehnsw_engine_basic_fast_disk<float>>(m, "expANN")
+			.def(py::init<>())
+			.def("name", &ehnsw_engine_basic_fast_disk<float>::name)
+			.def("param_list", &ehnsw_engine_basic_fast_disk<float>::param_list)
+			.def("store_vector", &ehnsw_engine_basic_fast_disk<float>::store_vector)
+			.def("build", &ehnsw_engine_basic_fast_disk<float>::build)
+			.def("query_k", &ehnsw_engine_basic_fast_disk<float>::query_k);
+}
