cc1cd3aaed6c8e1768ddcaae4e315dcecf50c920
jacketsj <jacketsj@gmail.com>
2024-01-08 17:23:22 -0800

    Change params run

--- src/bench_runner.h
+++ src/bench_runner.h
@@ -141,19 +141,19 @@ bench_data_manager perform_benchmarks(test_dataset_t ds, size_t num_threads) {
 			job_lists;
 
 	for (size_t k = 60; k <= 60; k += 20) {
-		for (size_t num_for_1nn = 1; num_for_1nn <= 5; num_for_1nn += 2) { // 5
+		for (size_t num_for_1nn = 1; num_for_1nn <= 6; num_for_1nn += 1) { // 5
 			for (bool use_cuts : {false}) {
 				if (false) {
 					ADD_JOB(ensg_engine<float>, k, num_for_1nn, use_cuts, 1.0f);
 				}
 			}
-			for (size_t edge_count_search_factor : {2}) { // 3
+			for (size_t edge_count_search_factor : {4}) { // 3
 				for (bool use_cuts : {false}) {
 					if (true) {
 						ADD_JOB(ehnsw_engine_basic_fast<float>, k, 2 * k, num_for_1nn,
 										k * edge_count_search_factor, use_cuts);
 					}
-					for (size_t min_cluster_size : {32}) {										 // 32
+					for (size_t min_cluster_size : {8, 32, 128, 512}) {				 // 32
 						for (size_t max_cluster_size : {min_cluster_size * 4}) { // * 4
 							for (bool very_early_termination : {false}) {
 								for (bool use_clusters_data : {true}) {
