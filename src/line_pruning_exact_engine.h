85e13e8539520cc2475e232a1ea7257c80bccf69
jacketsj <jacketsj@gmail.com>
2024-03-15 06:28:25 -0700

    Initial parts of line pruning exact engine implementation

---
+++ src/line_pruning_exact_engine.h
@@ -0,0 +1,112 @@
+#pragma once
+
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <random>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "ann_engine.h"
+#include "robin_hood.h"
+#include "topk_t.h"
+
+namespace {
+
+template <typename A, typename B> auto dist2(const A& a, const B& b) {
+	return (a - b).squaredNorm();
+}
+} // namespace
+
+struct line_pruning_exact_engine_config {
+	size_t brute_force_size;
+	size_t line_count;
+	line_pruning_exact_engine_config(size_t _brute_force_size, size_t _line_count)
+			: brute_force_size(_brute_force_size), line_count(_line_count) {}
+};
+
+template <typename T>
+struct line_pruning_exact_engine
+		: public ann_engine<T, line_pruning_exact_engine<T>> {
+	using fvec = typename vec<T>::Underlying;
+	std::random_device rd;
+	std::mt19937 gen;
+	std::uniform_real_distribution<> distribution;
+	struct line {
+		// TODO a is completely unnecessary I think
+		fvec a, dir;
+		line(const fvec& a, const fvec& b) : a(a), dir((b - a).normalized()) {}
+		T proj(fvec v) const { return dot(v - a, dir); }
+	};
+	struct line_with_points {
+		line l;
+		std::set<std::pair<T, size_t>> sorted_elems;
+		line_with_points(const line& l, const std::vector<size_t>& elems,
+										 const std::vector<fvec>& entries)
+				: l(l) {
+			for (size_t i : elems) {
+				sorted_elems.emplace(l.proj(entries[i]), i);
+			}
+		}
+		float get_lb(const fvec& v) const {
+			auto projected_v = l.proj(v);
+			auto it = sorted_elems.lower_bound(std::make_pair(projected_v, 0));
+			auto it_dist = [&]() { return std::abs(it->first - projected_v); };
+			if (it == sorted_elems.begin())
+				return it_dist();
+			auto pit = std::prev(it);
+			auto pit_dist = [&]() { return std::abs(pit->first - projected_v); };
+			if (it == sorted_elems.end())
+				return pit_dist();
+			return std::min(it_dist(), pit_dist());
+		}
+	};
+	struct cluster_tree_node {
+		std::vector<size_t> elems;
+		std::vector<line_with_points> lines;
+		std::vector<size_t> children;
+		float get_lb(const fvec& v) {
+			// TODO when getting a lb, also return the corresponding index, so that it
+			// can be tested to see if it's a new best approximate nearest neighbour
+			// (and a new graph search can be initiated with it as a seed if so)
+			float best = 0;
+			for (const auto& lp : lines) {
+				best = std::max(best, lp.get_lb(v));
+			}
+			return best;
+		}
+	};
+	size_t brute_force_size;
+	size_t line_count;
+	std::vector<cluster_tree_node> cluster_tree;
+	line_pruning_exact_engine(line_pruning_exact_engine_config conf)
+			: rd(), gen(0), distribution(0, 1),
+				brute_force_size(conf.brute_force_size), line_count(conf.line_count) {}
+	using config = line_pruning_exact_engine_config;
+	std::vector<fvec> all_entries;
+	void _store_vector(const vec<T>& v);
+	void _build();
+	std::vector<size_t> _query_k(const vec<T>& v, size_t k);
+	const param_list_t _param_list() {
+		param_list_t pl;
+		add_param(pl, brute_force_size);
+		add_param(pl, line_count);
+		return pl;
+	}
+};
+
+template <typename T>
+void line_pruning_exact_engine<T>::_store_vector(const vec<T>& v0) {
+	auto v = v0.internal;
+	size_t v_index = all_entries.size();
+	all_entries.emplace_back(v);
+}
+
+template <typename T> void line_pruning_exact_engine<T>::_build() {}
+
+template <typename T>
+std::vector<size_t> line_pruning_exact_engine<T>::_query_k(const vec<T>& q0,
+																													 size_t k) {}
