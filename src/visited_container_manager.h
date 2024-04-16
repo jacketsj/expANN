9c19e74a3206bb76173c5040cc1827a778c013ca
jacketsj <jacketsj@gmail.com>
2024-04-16 12:16:01 -0700

    Make visited container manager thread-safe

--- src/visited_container_manager.h
+++ src/visited_container_manager.h
@@ -13,51 +13,48 @@ struct VisitedContainer {
 			: taken(false), visited(data_size), visited_recent() {}
 };
 struct VisitedContainerRef {
-	std::vector<std::unique_ptr<VisitedContainer>>& visited_containers_ref;
 	std::mutex& visited_containers_mutex;
 	size_t index;
 	bool thread_safe;
-	VisitedContainer& vref;
-	VisitedContainerRef(
-			std::vector<std::unique_ptr<VisitedContainer>>& visited_containers_ref,
-			std::mutex& visited_containers_mutex, size_t index, size_t data_size,
-			bool thread_safe = false)
-			: visited_containers_ref(visited_containers_ref),
-				visited_containers_mutex(visited_containers_mutex), index(index),
-				thread_safe(thread_safe), vref(*visited_containers_ref[index]) {
-		if (vref.taken == true) {
+	std::shared_ptr<VisitedContainer> vref;
+	VisitedContainerRef(std::shared_ptr<VisitedContainer> visited_container_ref,
+											std::mutex& visited_containers_mutex, size_t index,
+											size_t data_size, bool thread_safe = false)
+			: visited_containers_mutex(visited_containers_mutex), index(index),
+				thread_safe(thread_safe), vref(visited_container_ref) {
+		if (vref->taken == true) {
 			throw std::runtime_error(
 					"Overlapping refs detected! Not thread safe! index=" +
 					std::to_string(index));
 		}
-		if (vref.visited.size() < data_size) {
-			vref.visited.resize(data_size);
+		if (vref->visited.size() < data_size) {
+			vref->visited.resize(data_size);
 		}
-		vref.taken = true;
+		vref->taken = true;
 	}
 	inline bool Visit(const size_t& data_index) {
-		if (vref.visited[data_index]) {
+		if (vref->visited[data_index]) {
 			return false;
 		}
-		vref.visited[data_index] = true;
-		vref.visited_recent.emplace_back(data_index);
+		vref->visited[data_index] = true;
+		vref->visited_recent.emplace_back(data_index);
 		return true;
 	}
 	~VisitedContainerRef() {
-		for (const size_t& data_index : vref.visited_recent) {
-			vref.visited[data_index] = false;
+		for (const size_t& data_index : vref->visited_recent) {
+			vref->visited[data_index] = false;
 		}
-		vref.visited_recent.clear();
+		vref->visited_recent.clear();
 		if (!thread_safe) {
 			std::lock_guard<std::mutex> lock(visited_containers_mutex);
-			vref.taken = false;
+			vref->taken = false;
 		} else {
-			vref.taken = false;
+			vref->taken = false;
 		}
 	}
 };
 struct VisitedContainerManager {
-	std::vector<std::unique_ptr<VisitedContainer>> visited_containers;
+	std::vector<std::shared_ptr<VisitedContainer>> visited_containers;
 	std::mutex visited_containers_mutex;
 	void resize_visit_containers(size_t num_threads_lower_bound,
 															 size_t data_size) {
@@ -65,16 +62,18 @@ struct VisitedContainerManager {
 			std::lock_guard<std::mutex> lock(visited_containers_mutex);
 			while (visited_containers.size() < num_threads_lower_bound) {
 				visited_containers.emplace_back(
-						std::make_unique<VisitedContainer>(data_size));
+						std::make_shared<VisitedContainer>(data_size));
 			}
 		}
 	}
 	VisitedContainerRef get_visitref(std::optional<size_t> thread_index,
 																	 size_t data_size) {
 		if (thread_index.has_value()) {
-			resize_visit_containers(thread_index.value() + 1, data_size);
-			return VisitedContainerRef(visited_containers, visited_containers_mutex,
-																 thread_index.value(), data_size, true);
+			size_t ti = thread_index.value() + 1;
+			resize_visit_containers(ti, data_size);
+			return VisitedContainerRef(visited_containers[thread_index.value()],
+																 visited_containers_mutex, thread_index.value(),
+																 data_size, true);
 		}
 		std::lock_guard<std::mutex> lock(visited_containers_mutex);
 		size_t index = 0;
@@ -87,7 +86,7 @@ struct VisitedContainerManager {
 			visited_containers.emplace_back(
 					std::make_unique<VisitedContainer>(data_size));
 		}
-		return VisitedContainerRef(visited_containers, visited_containers_mutex,
-															 index, data_size);
+		return VisitedContainerRef(visited_containers[index],
+															 visited_containers_mutex, index, data_size);
 	}
 };
