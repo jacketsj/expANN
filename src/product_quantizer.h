ffcca411bf0853c87d08fdb52635007af4503a79
jacketsj <jacketsj@gmail.com>
2024-04-27 01:49:39 -0700

    Use codes_per_subspace matrix structure to store codes

--- src/product_quantizer.h
+++ src/product_quantizer.h
@@ -14,9 +14,12 @@ class product_quantizer_scorer : public quantized_scorer {
 	static constexpr size_t NUM_CENTROIDS = 16;
 
 	// using codes_t = std::array<uint8_t, NUM_SUBSPACES>;
-	using codes_t = uint64_t;
+	//  using codes_t = uint64_t;
 
-	const std::vector<std::vector<codes_t>>& codes;
+	// const std::vector<std::vector<codes_t>>& codes;
+	// const std::vector<std::vector<uint8_t>>& codes_per_subspace;
+	const std::vector<Eigen::Matrix<uint8_t, Eigen::Dynamic, NUM_SUBSPACES>>&
+			codes_per_subspace;
 
 	using centroids_t =
 			Eigen::Matrix<float, NUM_CENTROIDS, Eigen::Dynamic, Eigen::RowMajor>;
@@ -26,12 +29,17 @@ class product_quantizer_scorer : public quantized_scorer {
 
 public:
 	product_quantizer_scorer(
-			const fvec& _query, const std::vector<std::vector<codes_t>>& _codes,
+			const fvec& _query,
+			// const std::vector<std::vector<codes_t>>& _codes,
+			// const std::vector<std::vector<uint8_t>>& _codes_per_subspace,
+			const std::vector<Eigen::Matrix<uint8_t, Eigen::Dynamic, NUM_SUBSPACES>>&
+					_codes_per_subspace,
 			const std::vector<centroids_t>& _sub_centroids_compact)
 			//,const std::vector<std::array<std::array<Eigen::VectorXf,
 			// NUM_CENTROIDS>, 														 NUM_SUBSPACES>>&
 			// _sub_centroids)
-			: query(_query), codes(_codes),
+			: query(_query), codes_per_subspace(_codes_per_subspace),
+				// codes(_codes),
 				sub_centroids_compact(_sub_centroids_compact)
 	//,sub_centroids(_sub_centroids)
 	{}
@@ -120,6 +128,7 @@ public:
 		}
 		// Use pq table to compute distances for to_filter_offsets
 		if (false) {
+			/*
 			for (size_t entry_index = 0; entry_index < to_filter_offsets.size();
 					 ++entry_index) {
 				float res = 0;
@@ -138,7 +147,9 @@ public:
 					distances.emplace_back(res);
 				}
 			}
-		} else {
+			*/
+		} else if (false) {
+			/*
 			Eigen::Matrix<float, NUM_CENTROIDS, NUM_SUBSPACES, Eigen::ColMajor>
 					distance_table_colmaj = distance_table;
 			// Eigen::VectorX<uint64_t> codes_table(to_filter_offsets.size());
@@ -169,6 +180,32 @@ public:
 					distances.emplace_back(res);
 				}
 			}
+			*/
+		} else {
+			Eigen::Matrix<float, NUM_CENTROIDS, NUM_SUBSPACES, Eigen::ColMajor>
+					distance_table_colmaj = distance_table;
+			size_t adj_size = codes_per_subspace[cur_vert].size() / NUM_SUBSPACES;
+
+			// Eigen::Map<Eigen::Matrix<uint8_t, Eigen::Dynamic, NUM_SUBSPACES,
+			//												 Eigen::ColMajor>>
+			// Eigen::Matrix<uint8_t, Eigen::Dynamic, NUM_SUBSPACES,
+			//							Eigen::ColMajor>
+			//		codes_table; //(codes_per_subspace[cur_vert].data(), adj_size,
+			// NUM_SUBSPACES);
+			Eigen::VectorXf distance_vec = Eigen::VectorXf::Zero(adj_size);
+			for (size_t cursubspace = 0; cursubspace < NUM_SUBSPACES; ++cursubspace) {
+				distance_vec += distance_table_colmaj.col(cursubspace)(
+						codes_per_subspace[cur_vert].col(cursubspace));
+			}
+			for (size_t entry_index = 0; entry_index < to_filter_offsets.size();
+					 ++entry_index) {
+				size_t entry_offset = to_filter_offsets[entry_index];
+				auto res = distance_vec[entry_offset];
+				if (res < cutoff) {
+					filtered_out.emplace_back(to_filter[entry_index]);
+					distances.emplace_back(res);
+				}
+			}
 		}
 	}
 };
@@ -181,8 +218,11 @@ class product_quantizer : public quantizer {
 	static constexpr size_t NUM_CENTROIDS = 16;
 
 	// using codes_t = std::array<uint8_t, NUM_SUBSPACES>;
-	using codes_t = uint64_t;
-	std::vector<std::vector<codes_t>> codes;
+	// using codes_t = uint64_t;
+	// std::vector<std::vector<codes_t>> codes;
+	// std::vector<std::vector<uint8_t>> codes_per_subspace;
+	std::vector<Eigen::Matrix<uint8_t, Eigen::Dynamic, NUM_SUBSPACES>>
+			codes_per_subspace;
 	using centroids_t =
 			Eigen::Matrix<float, NUM_CENTROIDS, Eigen::Dynamic, Eigen::RowMajor>;
 	std::vector<centroids_t> sub_centroids_compact;
@@ -213,10 +253,15 @@ public:
 		sub_centroids_compact = std::vector<centroids_t>(
 				unquantized.size(), centroids_t::Zero(NUM_CENTROIDS, dimension));
 		// sub_centroids.resize(unquantized.size());
-		codes.resize(unquantized.size());
+		codes_per_subspace.resize(unquantized.size());
+		// codes.resize(unquantized.size());
 		for (size_t base = 0; base < unquantized.size(); ++base) {
 			// codes[base].resize(adj[base].size());
-			codes[base] = std::vector<codes_t>(adj[base].size(), 0);
+			// codes[base] = std::vector<codes_t>(adj[base].size(), 0);
+			// codes_per_subspace[base].resize(adj[base].size() * NUM_SUBSPACES);
+			codes_per_subspace[base] =
+					Eigen::Matrix<uint8_t, Eigen::Dynamic, NUM_SUBSPACES>(
+							adj[base].size(), NUM_SUBSPACES);
 		}
 		std::cout << "Running k-means on each neighbourhood" << std::endl;
 		std::random_device rd;
@@ -258,22 +303,26 @@ public:
 					//		current_centroids[curcentroid];
 				}
 				for (size_t neighbour = 0; neighbour < adj[base].size(); ++neighbour) {
-					// codes[base][neighbour][cursubspace] =
-					// uint8_t(sub_labels[neighbour]);
 					if (sub_labels[neighbour] >= 16) {
 						throw std::runtime_error("Sub_label too big");
 					}
-					codes[base][neighbour] |= ((0b1111 & sub_labels[neighbour])
-																		 << (cursubspace * SUBSPACE_INDEX_SIZE));
+					codes_per_subspace[base](neighbour, cursubspace) =
+							uint8_t(sub_labels[neighbour]);
+					// codes_per_subspace[base][neighbour + cursubspace *
+					// adj[base].size()] = 		uint8_t(sub_labels[neighbour]);
+					//  codes[base][neighbour] |= ((0b1111 & sub_labels[neighbour])
+					//													 << (cursubspace * SUBSPACE_INDEX_SIZE));
 					//[cursubspace] = uint8_t(sub_labels[neighbour]);
 					//((codes[base][neighbour] >> (cursubspace * 4)) & 0b1111)
 				}
+				/*
 				for (size_t neighbour = 0; neighbour < adj[base].size(); ++neighbour) {
 					if (((codes[base][neighbour] >> (cursubspace * SUBSPACE_INDEX_SIZE)) &
 							 0b1111) != sub_labels[neighbour]) {
 						throw std::runtime_error("The labels don't convert properly");
 					}
 				}
+				*/
 			}
 		}
 		std::cout << "Done running k-means on each neighbourhood" << std::endl;
@@ -282,6 +331,7 @@ public:
 	generate_scorer(const fvec& query) override {
 		auto padded_query = pad(query);
 		return std::make_unique<product_quantizer_scorer>(
-				padded_query, codes, sub_centroids_compact); //, sub_centroids);
+				padded_query, codes_per_subspace,
+				sub_centroids_compact); //, sub_centroids);
 	}
 };
