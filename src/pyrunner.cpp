83e3989bb7d91bd8014e3b46a2d185e315166666
jacketsj <jacketsj@gmail.com>
2024-04-27 20:10:49 -0700

    Add faster query function

--- src/pyrunner.cpp
+++ src/pyrunner.cpp
@@ -52,6 +52,12 @@ PYBIND11_MODULE(MODULE_NAME, m) {
 			.def("store_vector", &antitopo_engine<float>::store_vector)
 			.def("build", &antitopo_engine<float>::build)
 			.def("query_k", &antitopo_engine<float>::query_k)
+			.def("query_k_numpy",
+					 [](antitopo_engine<float>& engine, py::array_t<float> input_array,
+							size_t k) {
+						 return engine.query_k(
+								 vec<float>(convert_numpy_to_eigen_padded(input_array)), k);
+					 })
 			.def("set_ef_search", &antitopo_engine<float>::set_ef_search);
 }
 } // namespace MODULE_NAME
