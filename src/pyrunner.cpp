18307969d304aedb9c9c29a787e9bdf26b60c94c
jacketsj <jacketsj@gmail.com>
2024-04-27 18:01:28 -0700

    Allow initialization of vec type using (unpadded) numpy vectors

--- src/pyrunner.cpp
+++ src/pyrunner.cpp
@@ -1,16 +1,42 @@
+#include <Eigen/Dense>
+#include <pybind11/eigen.h>
+#include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "ann_engine.h"
 #include "antitopo_engine.h"
+#include "vec.h"
 
 namespace py = pybind11;
 
 namespace MODULE_NAME {
+vec<float>::Underlying convert_numpy_to_eigen(py::array_t<float> input_array) {
+	py::buffer_info buf_info = input_array.request();
+	return Eigen::Map<vec<float>::Underlying>(static_cast<float*>(buf_info.ptr),
+																						buf_info.shape[0]);
+}
+vec<float>::Underlying
+convert_numpy_to_eigen_padded(py::array_t<float> input_array) {
+	py::buffer_info buf_info = input_array.request();
+#ifdef DIM
+	size_t dimension = DIM;
+#else
+	size_t dimension = buf_info.shape[0];
+#endif
+	auto mapped_input = Eigen::Map<const Eigen::VectorXf>(
+			static_cast<float*>(buf_info.ptr), buf_info.shape[0]);
+	vec<float>::Underlying ret = vec<float>::Underlying::Zero(dimension);
+	ret.head(buf_info.shape[0]) = mapped_input.head(buf_info.shape[0]);
+	return ret;
+}
+
 PYBIND11_MODULE(MODULE_NAME, m) {
 	py::class_<vec<float>>(m, "Vec", py::module_local())
 			.def(py::init<>())
 			.def(py::init<const std::vector<float>&>())
+			.def(py::init([](py::array_t<float> input_array) {
+				return vec<float>(convert_numpy_to_eigen_padded(input_array));
+			}))
 			.def("data",
 					 [](vec<float>& v) {
 						 return py::memoryview::from_buffer(v.data(), {v.size()},
